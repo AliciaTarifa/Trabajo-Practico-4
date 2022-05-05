3b98f417aa6c799ae48da0848719accbb63643fe
Alicia <alicia.tarifa.8@gmail.com>
2022-05-05 19:35:44 -0300

    Ejercicio 01

--- src/Ejercicios/Persona.java
+++ src/Ejercicios/Persona.java
@@ -11,11 +11,29 @@ public class Persona {
 
 	/**
 	 * @param args
-	 * Crear una clase llamada Persona con los atributos: Nombre, Apellido, DNI, Domicilio. Con  los métodos: Constructor y un método que permita mostrar la información de la persona.  
+	 * Crear una clase llamada Persona con los atributos: Nombre, Apellido, DNI, Domicilio. Con  los métodos: Constructor 
+	 * y un método que permita mostrar la información de la persona.  
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		String nombre;
+		String apellido;
+		int dni;
+		String domicilio;
+		
+		public Persona (String nombre, String apellido, int dni, String domicilio) {
+			this. nombre = nombre;
+			this. apellido = apellido;
+			this. dni = dni;
+			this. domicilio = domicilio;
+		}
+		
+		public void mostrar() {
+			System.out.print("nombre:"+ this.nombre);
+			System.out.print("apellido:"+this.apellido);
+			System.out.print("dni:"+ this.dni);
+			System.out.print("domicilio:"+ this.domicilio);
+		}
 
 	}
 
-}
+
