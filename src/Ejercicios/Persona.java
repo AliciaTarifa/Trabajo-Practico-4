443649fdc08b6705b63f88e2ffd7d6869a884967
Alicia <alicia.tarifa.8@gmail.com>
2022-05-05 19:34:13 -0300

    Ejercicio 01

---
+++ src/Ejercicios/Persona.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Persona {
+
+	/**
+	 * @param args
+	 * Crear una clase llamada Persona con los atributos: Nombre, Apellido, DNI, Domicilio. Con  los métodos: Constructor y un método que permita mostrar la información de la persona.  
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
